829f4c7858d8a6dca617ec1911f0df06a3f24526
abhishek981996 <abhishektiwari981996@gmail.com>
2017-04-20 12:03:34 +0530

    changes done in urls.py

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<year>\w{4})/(?P<month>\w{1})',
+    url(r'^(?P<year>\w{4})/(?P<month>\d+)',
         views.calendars, name='calendar'),
     url(r'^$', views.today, name='calendar_now')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -6,13 +6,32 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+# helper function for clculating the previous year etc
+def calculate(year, month):
+    if month == 1:
+        prev_year = year - 1
+        prev_month = 12
+        nex_year = year
+        nex_month = month + 1
+    elif month == 12:
+        prev_year = year
+        prev_month = month - 1
+        nex_year = year + 1
+        nex_month = 1
+    else:
+        prev_year = year
+        prev_month = month - 1
+        nex_year = year
+        nex_month = month + 1
+    return prev_year, prev_month, nex_year, nex_month
+
+
 def calendars(request, year, month):
     tc = calendar.HTMLCalendar()
     str = tc.formatmonth(int(year), int(month))
-    prev_year = int(year)
-    prev_month = int(month) - 1
-    nex_year = int(year)
-    nex_month = int(month) + 1
+    prev_year, prev_month, nex_year, nex_month = calculate(
+        int(year), int(month))
+    print '%s,%s,%s,%s' % (prev_year, prev_month, nex_year, nex_month)
     return render(request, 'home.html', {
         'htmlCalendar': str,
         'prev_year': prev_year,
@@ -26,10 +45,9 @@ def today(request):
     time = datetime.datetime.now()
     tc = calendar.HTMLCalendar()
     str = tc.formatmonth(time.year, time.month)
-    prev_year = time.year
-    prev_month = time.month - 1
-    nex_year = time.year
-    nex_month = time.month + 1
+    prev_year, prev_month, nex_year, nex_month = calculate(
+        int(time.year), int(time.month))
+
     return render(request, 'home.html', {
         'htmlCalendar': str,
         'prev_year': prev_year,
